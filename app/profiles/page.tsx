e6d00bd75d34d7099916b70e8a2c2c99656b783c
tmddv <105033488+tmddv@users.noreply.github.com>
2024-08-23 04:32:54 +0900

    [Feat] 프로필

---
+++ app/profiles/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8"></div>
+    </main>
+  );
+}
