9d4c618d0b2664ae1aeb5d7637b5202293748f82
tmddv <105033488+tmddv@users.noreply.github.com>
2024-08-24 01:38:35 +0900

    [Feat] 미들웨어 및 쿠키검증,재발급 함수

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,4 +1,6 @@
-const nextUrl= process.env.NEXT_URL
+import { getToken } from "./cookie";
+
+const url = process.env.SPRING_URL
 
 export async function signIn() {
 }
@@ -7,17 +9,30 @@ export async function signOut() {
 export async function join() {
 }
 
-export async function tokenTest(token:string){
-    try{
-        const response = await fetch(`${nextUrl}/api/cookie/set/?token=${token}`, {
+
+export async function tokenTest() {
+    try {
+        const headers: { 'Content-Type': string; 'Cache-Control': string; 'Authorization'?: string } = {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache'
+        };
+        const token = await getToken();
+        const value = token?.value;
+        if (value) {
+            headers['Authorization'] = `Bearer ${value}`;
+        }
+        const response = await fetch(`${url}/auth/check`, {
             method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
+            headers: headers
         });
+        const status = response.status
+        if(status !=200){
+            return false
+        }
         const data = await response.json();
+        return data;
     }
-    catch(error){
+    catch (error) {
 
     }
 }
