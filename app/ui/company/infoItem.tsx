07a1660fbae48f64eb48255a5764e2cbb0fa4f47
tmddv <105033488+tmddv@users.noreply.github.com>
2024-08-24 01:38:52 +0900

    [Style] 반응형 추가작업

--- app/ui/company/infoItem.tsx
+++ app/ui/company/infoItem.tsx
@@ -5,15 +5,15 @@ export default async function InfoItem({src,label,data}:{src:string;label:string
         <div className="pb-4">
                     <div className="flex justify-between py-2">
                       <div className="flex">
-                        <Image
+                        <div className="flex flex-col justify-center"><Image
                           src={src}
                           width={24}
                           height={24}
                           alt="time icon"
-                        />
-                        <div className="ml-2 text-gray-700 text-base my-auto">{label}</div>
+                        /></div>
+                        <div className="ml-2 text-gray-700 text-sm md:text-base my-auto">{label}</div>
                       </div>
-                      <div className="text-lg my-auto">{data}</div>
+                      <div className="text-base md:text-lg my-auto">{data}</div>
                     </div>
                   </div>
     )
