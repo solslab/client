90972a2d7125d6e14404d857ca26669a48fc8283
tmddv <105033488+tmddv@users.noreply.github.com>
2024-08-24 01:37:15 +0900

    [Feat] 마지막경로 세팅 컴포넌트

---
+++ app/ui/lastPathSetter.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect } from "react";
+import { usePathname} from "next/navigation";
+import {
+  updateLastRoute,
+} from "@/app/lib/cookie";
+
+const exception = ["/login"];
+
+export default function LastPathSetter() {
+  const pathName = usePathname();
+  useEffect(() => {
+    if (!exception.includes(pathName)) {
+      updateLastRoute(pathName);
+    }
+  }, [pathName]); // 라우트가 변경될 때마다 실행
+
+  return <></>;
+}
