35ca2d15bcbdf36c0c731b3753092da093d0796c
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-01 05:46:28 +0900

    [Fix] class 순서

--- app/ui/container.tsx
+++ app/ui/container.tsx
@@ -4,5 +4,5 @@ export default function Container({
   children: React.ReactNode;
   className?:string;
 }>) {
-  return <div className={`max-w-6xl w-full mx-auto px-4 ${className}`}>{children}</div>;
+  return <div className={` w-full max-w-6xl  mx-auto px-4 ${className}`}>{children}</div>;
 }
