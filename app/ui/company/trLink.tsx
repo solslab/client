48b60709253dbcc69a2d83a3b83801c8035640ac
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-01 05:47:48 +0900

    [Feat] 후기작성 바로가기

---
+++ app/ui/company/trLink.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function TrLink() {
+  return (
+
+      <Link href='/testReview' className="text-2xl text-white bg-main-base rounded-2xl flex justify-center items-center w-80 h-20">
+        코딩 테스트 후기 작성하기
+      </Link>
+
+  );
+}
