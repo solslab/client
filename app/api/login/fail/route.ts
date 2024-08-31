6ee80a42249ccd3572419b74fa2741fff1e671dc
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-01 04:36:19 +0900

    [Fix] 로그인실패

--- app/api/login/fail/route.ts
+++ app/api/login/fail/route.ts
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 export async function GET(request:Request) {
+    const url = process.env.NEXT_URL || ''
     console.log('로그인 실패')
-    redirect(process.env.NEXT_URL)
+    redirect(url)
     
 }
