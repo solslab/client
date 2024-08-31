a40da6b195302ac9f018b73dbd8f3156d7e8c88d
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-01 04:30:20 +0900

    [Feat] 미들웨어 matcher및 로직 추가

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,73 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { tokenTest } from './app/lib/auth';
+import { infoCheck } from './app/lib/utils';
+import { getLastRoute } from './app/lib/cookie';
+
+
 export default async function middleware(request: NextRequest) {
-    const cookie = request.cookies.get("sols-accessToken");
+    const nextUrl = process.env.NEXT_URL
     const requestUrl = request.nextUrl.href;
-    const response = NextResponse.redirect(requestUrl);
-    if(cookie){
-       const data = await tokenTest();
-        if(data){
-            if(data.new_token){
-                response.cookies.set("sols-accessToken",data.new_token);
-                return response;
+    const pathName = request.nextUrl.pathname;
+    const token = await tokenTest();
+    const lastPathCookie = await getLastRoute();
+    const lastPath = lastPathCookie?.value || '/';
+    let response;
+    if (pathName.includes('suggestion')) {
+        if (token) {
+            response = NextResponse.next();
+
+            if (token.new_token) {
+                const clearToken = token.new_token.replace('Bearer ', '');
+                response.cookies.set("sols-accessToken", clearToken);
             }
         }
-        else{
+        else {
+            response = NextResponse.redirect(nextUrl + '/login');
             response.cookies.delete("sols-accessToken")
-            return response;
         }
+        return response;
+    }
+    else if (pathName.startsWith('/company')) {
+        if (token) {
+            if (token.new_token) {
+                response = NextResponse.next();
+                const clearToken = token.new_token.replace('Bearer ', '');
+                response.cookies.set("sols-accessToken", clearToken);
+            }
+        }
+        else if (token == false) {
+            response = NextResponse.redirect(requestUrl);
+            response.cookies.delete("sols-accessToken")
+        }
+        return response;
+    }
+    else if (pathName.startsWith('/profiles')) {
+        if (token) {
+            response = NextResponse.next();
+            if (pathName.startsWith('/profiles/additional')) {
+                const infoChecked: boolean = await infoCheck();
+                if (infoChecked)
+                    response = NextResponse.redirect(nextUrl + lastPath);
+            }
+
+            if (token.new_token) {
+                const clearToken = token.new_token.replace('Bearer ', '');
+                response.cookies.set("sols-accessToken", clearToken);
+            }
+        }
+        else {
+            response = NextResponse.redirect(nextUrl + '/login');
+            response.cookies.delete("sols-accessToken")
+        }
+        return response;
     }
 
-    }
-    export const config = {
-        matcher: ['/company/:id*'], // 동적 경로 매칭
-      };
-  
 
-  
-  
+
+}
+export const config = {
+    matcher: ['/company/:id*', '/profiles/:path*']
+};
+
+
+
