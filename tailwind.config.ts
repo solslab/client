83d5e615db72bc199ac335ffb89e6d025f4759ed
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-01 04:28:54 +0900

    [Chore] config 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,8 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/preline/preline.js',
+
   ],
   theme: {
     extend: {
@@ -16,8 +18,35 @@ const config: Config = {
       fontFamily: {
         pretendard: ['Pretendard', 'sans-serif'],
       },
+      height: {
+        '46': '46rem',
+      },
+      width: {
+        '160': '58rem',
+        '120': '48rem'
+      },
+      colors: {
+        'custom-purple-1': '#2C2B38',
+        'custom-purple-2': '#3E2E51',
+        'text-base': '#4A4C4F',
+        'gray-10': '#ECEEF0',
+        "gray-20": "#E8E9EB",
+        "gray-30": "#E3E4E6",
+        "gray-40": "#D7D9DB",
+        "gray-50": "#CCCDD0",
+        "gray-60": "#B7B9BB",
+        "gray-70": "#A1A2A5",
+        "gray-80": "#75787A",
+        "gray-90": "#4A4C4F",
+        "main-base": "#403CE5",
+        "main-light": "#E8E8FA",
+        "bg-base": "#F8F9FB"
+
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 };
 export default config;
