f00fe31871575ea521d6b6d89719704f6d4ce64a
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-01 04:32:29 +0900

    [Refactor] 최초 추가정보입력 리팩토링

--- app/profiles/additional/page.tsx
+++ app/profiles/additional/page.tsx
@@ -1,95 +1,32 @@
 "use client";
 import ComboBox from "@/app/ui/comboBox";
-import clsx from "clsx";
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
-import { platform } from "os";
-import { useEffect, useState } from "react";
-const skillList = [
-  "C",
-  "C++",
-  "C#",
-  "Java",
-  "JavaScript",
-  "Kotlin",
-  "Python",
-  "Go",
-  "Ruby",
-  "Scala",
-  "Swift",
-  "SQL",
-  "Oracle"
-];
-const platformList = [
-  { platform: "선택안함", level: [] },
-  {
-    platform: "백준 (solved.ac)",
-    level: [
-      { label: "브론즈5", value: 1 },
-      { label: "브론즈4", value: 2 },
-      { label: "브론즈3", value: 3 },
-      { label: "브론즈2", value: 4 },
-      { label: "브론즈1", value: 5 },
-      { label: "실버5", value: 6 },
-      { label: "실버4", value: 7 },
-      { label: "실버3", value: 8 },
-      { label: "실버2", value: 9 },
-      { label: "실버1", value: 10 },
-      { label: "골드5", value: 11 },
-      { label: "골드4", value: 12 },
-      { label: "골드3", value: 13 },
-      { label: "골드2", value: 14 },
-      { label: "골드1", value: 15 },
-      { label: "플레5", value: 16 },
-      { label: "플레4", value: 17 },
-      { label: "플레3", value: 18 },
-      { label: "플레2", value: 19 },
-      { label: "플레1", value: 20 },
-      { label: "다이아5", value: 26 },
-      { label: "다이아4", value: 27 },
-      { label: "다이아3", value: 28 },
-      { label: "다이아2", value: 29 },
-      { label: "다이아1", value: 30 },
-      { label: "루비5", value: 31 },
-      { label: "루비4", value: 32 },
-      { label: "루비3", value: 33 },
-      { label: "루비2", value: 34 },
-      { label: "루비1", value: 35 },
-      { label: "마스터 (이상)", value: 36 },
-    ],
-  },
-  {
-    platform: "프로그래머스",
-    level: [
-      { label: "레벨1", value: 4 },
-      { label: "레벨2", value: 8 },
-      { label: "레벨3", value: 13 },
-      { label: "레벨4", value: 19 },
-      { label: "레벨5", value: 28 },
-    ],
-  },
-  {
-    platform: "CodeForces",
-    level: [
-      { label: "0~800", value: 5 },
-      { label: "801~1200", value: 8 },
-      { label: "1201~1400", value: 12 },
-      { label: "1401~1600", value: 14 },
-      { label: "1601~1900", value: 17 },
-      { label: "1901~2100", value: 20 },
-      { label: "2101~", value: 28 },
-    ],
-  },
-];
-const feildList = ["aasds", "vqwdqb", "cvdve", "dqwdqs", "qwdfqf", "gfqdssd"];
+import Container from "@/app/ui/container";
+import NextButton from "@/app/ui/nextbutton";
+import PrevButton from "@/app/ui/prevButton";
+import LanguageToggleButton from "@/app/ui/profile/languageToggleButton";
+import { SKILLS, PLATFORMLIST, FEILDLIST } from "@/app/lib/constants";
+import { startTransition, useActionState, useEffect, useState } from "react";
+import {
+  AdditionalInformationState,
+  updateAdditionalInformation,
+} from "@/app/lib/actions";
+import FieldToggleButton from "@/app/ui/profile/fieldTogglebutton";
+import SubmitButton from "@/app/ui/submitButton";
 
 export default function Page() {
   const [step, setStep] = useState(1);
-  const [skills, setSkills] = useState(new Set());
+  const [skills, setSkills] = useState<Set<string>>(new Set());
   const [platform, setPlatform] = useState(0);
   const [level, setLevel] = useState(0);
-  const [field, setFeild] = useState([]);
+  const [field, setFeild] = useState<string[]>([]);
+  const initialState: AdditionalInformationState = {
+    message: null,
+    errors: {},
+  };
+  const [state, formAction] = useActionState(
+    updateAdditionalInformation,
+    initialState
+  );
   const addSkills = (skill: string) => {
     const newSet = new Set(skills);
     newSet.add(skill);
@@ -100,29 +37,56 @@ export default function Page() {
     newSet.delete(skill);
     setSkills(newSet);
   };
-  const handlePlatform = (e) => {
-    setPlatform(e.target.value);
-    if (e.target.value == 0) setLevel(0);
+  const handlePlatform = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value =Number(e.target.value)
+    setPlatform(value);
+    setLevel(PLATFORMLIST[value].level[0].value);
   };
-  const handleFeild = (value:string)=>{
-    const list = [...field];
+  const handleFeild = (value: string) => {
+    const list: string[] = [...field];
 
-    if(list.includes(value)){
-        const filteredList = list.filter(el => el !== value);
-        setFeild(filteredList)
+    if (list.includes(value)) {
+      const filteredList = list.filter((el) => el !== value);
+      setFeild(filteredList);
+    } else if (list.length < 5) {
+      list.push(value);
+      setFeild(list);
     }
-    else{
-        list.push(value)
-        setFeild(list)
-    }
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = {
+      al_platform: PLATFORMLIST[platform].code,
+      member_tier: level,
+      prefer_languages: skills,
+      prefer_industries: field,
+    };
+    const formData = new FormData(e.currentTarget);
+    formData.append("al_platform", PLATFORMLIST[platform].code);
+    formData.append("member_tier", level.toString());
+    formData.append("prefer_languages", Array.from(skills).toString());
+    formData.append("prefer_industries", field.toString());
 
-  }
+    startTransition(() => {
+      formAction(formData);
+    });
+  };
+
+  useEffect(() => {
+    const loadPreline = async () => {
+      await import("preline/preline");
+      window.HSStaticMethods.autoInit();
+    };
+
+    loadPreline();
+  }, [step]);
+  console.log(level,'@@@@')
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <Container className="pt-20">
       <div>
-        <div className="mx-auto max-w-2xl min-h-2 bg-gray-400">
+        <div className="mx-auto w-full h-4 bg-main-light rounded-full">
           <div
-            className="max-w-2xl min-h-2 bg-indigo-600"
+            className=" h-4 bg-main-base rounded-full"
             style={{
               width: `${(step / 3) * 100}%`,
               transition: "width 0.5s ease",
@@ -130,100 +94,113 @@ export default function Page() {
           ></div>
         </div>
       </div>
-      <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:py-32">
-        {step == 1 ? (
-          <div>
-            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              주로 사용하는 언어를 선택해주세요
-            </h1>
-            <p className="mt-6 text-sm leading-8 text-gray-600">
-              응시할 수 있는 공고를 추천해드릴게요!
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <ComboBox list={skillList} onClick={addSkills} />
-              {Array.from(skills).map((el) => (
-                <button
-                  onClick={() => removeSkills(el)}
-                  key={el}
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  {el}
-                </button>
-              ))}
-              <button
-                onClick={() => setStep(2)}
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                다음
-              </button>
+      <form onSubmit={handleSubmit}>
+        <div className="mx-auto  py-16 sm:py-24 lg:py-32">
+          {step == 1 ? (
+            <div>
+              <div className="h-48">
+                <h1 className="text-center text-2xl font-bold  text-gray-900 md:text-4xl">
+                  주로 사용하는 언어를 선택해주세요
+                </h1>
+                <p className="mt-6 text-lg md:text-2xl  text-gray-600 text-center">
+                  응시할 수 있는 공고를 추천해드릴게요!
+                </p>
+              </div>
+              <div className=" min-h-52 flex flex-col items-center justify-center gap-x-6">
+                <ComboBox list={SKILLS} onClick={addSkills} />
+                <div className="my-10">
+                  {Array.from(skills).map((el: string) => (
+                    <LanguageToggleButton
+                      key={el}
+                      text={el}
+                      onClick={() => removeSkills(el)}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className="flex justify-center">
+                <NextButton
+                  active={skills.size != 0}
+                  onClick={() => setStep(2)}
+                />
+              </div>
             </div>
-          </div>
-        ) : step == 2 ? (
-          <div>
-            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              회원님의 코딩 실력을 알려주세요!
-            </h1>
-            <p className="mt-6 text-sm leading-8 text-gray-600">
-              응시할 수 있는 공고를 추천해드릴게요!
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <select
-                value={platform}
-                onChange={(e) => handlePlatform(e)}
-                className="py-3  pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-              >
-                {platformList.map((platform, index) => (
-                  <option value={index} key={platform.platform}>
-                    {platform.platform}
-                  </option>
-                ))}
-              </select>
-              {platform != 0 && (
+          ) : step == 2 ? (
+            <div>
+              <div className="h-48">
+                <h1 className="text-center text-2xl font-bold  text-gray-900 md:text-4xl">
+                  회원님의 코딩 실력을 알려주세요!
+                </h1>
+                <p className="mt-6 text-lg md:text-2xl  text-gray-600 text-center">
+                  채용공고 추천 및 합격 예측을 위한 데이터랩에 사용될
+                  예정입니다.
+                </p>
+              </div>
+              <div className=" flex flex-col min-h-52 items-center justify-center gap-x-6">
                 <select
-                  value={level}
-                  onChange={(e) => setLevel(e.target.value)}
-                  className="py-3  pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                  value={platform}
+                  onChange={(e) => handlePlatform(e)}
+                  className="py-3 mb-6 pe-9 block w-80 max-w-full border-2 border-gray-60 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
                 >
-                  {platformList[platform].level.map((platform) => (
-                    <option value={platform.value} key={platform.label}>
-                      {platform.label}
+                  {PLATFORMLIST.map((platform, index) => (
+                    <option value={index} key={platform.platform}>
+                      {platform.platform}
                     </option>
                   ))}
                 </select>
-              )}
-              <button
-                onClick={() => setStep(3)}
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                다음
-              </button>
-            </div>
-          </div>
-        ) : (
-          <div>
-            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              취업 희망 분야를 알려주세요!
-            </h1>
-            <p className="mt-6 text-sm leading-8 text-gray-600">
-              응시할 수 있는 공고를 추천해드릴게요!
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-                {feildList.map((el)=>
-                 <button key={el} onClick={()=>handleFeild(el)} className={clsx(`rounded-md px-3.5 py-2.5 text-sm font-semibold bg-white text-black shadow-sm hover:bg-gray-300`,
-                    {
-                        'bg-gray-700 text-white': field.includes(el),
-                    }
-                 )}>
-                 {el}
-               </button>
+                {platform != 0 && (
+                  <select
+                    value={level}
+                    onChange={(e) => setLevel(Number(e.target.value))}
+                    className="py-3  pe-9 block w-80 max-w-full border-2 border-gray-60 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none "
+                  >
+                    {PLATFORMLIST[platform].level.map((platform) => (
+                      <option value={platform.value} key={platform.label}>
+                        {platform.label}
+                      </option>
+                    ))}
+                  </select>
                 )}
-              <button className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                다음
-              </button>
+              </div>
+              <div className="flex justify-center">
+                <PrevButton onClick={() => setStep(step - 1)} />
+                <NextButton
+                  onClick={() => setStep(3)}
+                  active={skills.size != 0}
+                />
+              </div>
             </div>
-          </div>
-        )}
-      </div>
-    </div>
+          ) : (
+            <div>
+              <div className="h-48">
+                <h1 className="text-center text-2xl font-bold  text-gray-900 md:text-4xl">
+                  어떤 기업에 가고싶으신가요?
+                </h1>
+                <p className="mt-6 text-lg md:text-2xl  text-gray-600 text-center">
+                  최대 5개까지 선택할 수 있습니다.
+                </p>
+              </div>
+              <div className="min-h-52">
+                <div className=" flex flex-wrap  justify-center max-w-xl mx-auto">
+                  {FEILDLIST.map((el) => (
+                    <FieldToggleButton
+                      key={el}
+                      text={el}
+                      onClick={() => handleFeild(el)}
+                      active={field.includes(el)}
+                    />
+                  ))}
+                </div>
+              </div>
+
+              <div className="flex justify-center">
+                <PrevButton onClick={() => setStep(step - 1)} />
+                <SubmitButton active={field.length!=0} onClick={()=>undefined}/>
+              </div>
+            </div>
+          )}
+        </div>
+      </form>
+    </Container>
   );
 }
