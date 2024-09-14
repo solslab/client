d5143cc968e8b06b2eafa293ac2257c94523edd5
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-14 17:47:54 +0900

    [Feat] 정보 수정 요청 링크

---
+++ app/ui/company/suggestionBtn.tsx
@@ -0,0 +1,13 @@
+'use client';
+import Link from 'next/link';
+
+export default function SuggestionBtn({company_id,position_id}:{company_id:string,position_id:string}) {
+	return (
+		<Link
+			href={`/company/${company_id}/suggestion?position=${position_id}`}
+			className="w-36 rounded-md bg-main-light px-6 py-3 text-main-base"
+		>
+			정보 수정 요청
+		</Link>
+	);
+}
