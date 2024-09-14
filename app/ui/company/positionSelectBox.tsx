851a3c8609f0de99e7ca0031fba6c46c361df13f
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-14 17:49:06 +0900

    [Style] 드랍 쉐도우 추가

--- app/ui/company/positionSelectBox.tsx
+++ app/ui/company/positionSelectBox.tsx
@@ -28,7 +28,7 @@ export default function PositionSelectBox({
     <select
       value={value}
       onChange={(e) =>handleChange(e)}
-      className="py-3 px-2  pe-9 block w-60 border text-text-base border-gray-30 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none0"
+      className="shadow-customShadow py-3 px-2  pe-9 block w-60 border text-text-base border-gray-30 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none0"
     >
       {positions.map((position, index) => (
         <option value={position.position_id} key={position.position_id}>
