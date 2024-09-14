ac9daaf1663052253c6c09d5e40465804bd439f7
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-14 18:00:57 +0900

    [Fix] Dom변경

--- app/profiles/edit/page.tsx
+++ app/profiles/edit/page.tsx
@@ -2,8 +2,6 @@ import SmallContainer from "@/app/ui/smallContainer";
 import { fetchProfile } from "@/app/lib/data";
 import { Profile } from "@/app/lib/definitions";
 import { findPlatformAndLabel } from "@/app/lib/utils";
-import LanguageBox from "@/app/ui/languageBox";
-import FieldBox from "@/app/ui/fieldBox";
 import ProfileEdit from "@/app/ui/profile/profileEdit";
 
 export default async function Page() {
@@ -13,10 +11,8 @@ export default async function Page() {
     profileData.member_tier
   ) || { platform: "알수없음", label: "" };
   return (
-    <main className="flex min-h-screen  justify-between ">
       <SmallContainer>
         <ProfileEdit profileData={profileData} />
       </SmallContainer>
-    </main>
   );
 }
