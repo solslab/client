1b1ff882de60116a593c165f937fd5b1ed29095f
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-14 17:44:54 +0900

    [Refactor] 이미지 최적화

--- app/ui/languageBox.tsx
+++ app/ui/languageBox.tsx
@@ -1,5 +1,6 @@
 
 import Image from "next/image";
+import { SKILLS_LOGO } from "../lib/constants";
 
 export default function LanguageBox({language}:{language:string}) {
   return (
@@ -8,12 +9,16 @@ export default function LanguageBox({language}:{language:string}) {
       className="border border-gray-30 rounded-3xl px-4 py-1 mx-2 my-2 flex"
     >
       <div className="mr-2 flex flex-col justify-center">
-        <Image
-          src={`/icons/${language}.png`}
+        {
+          SKILLS_LOGO[language]?.logo?
+          <Image
+          src={SKILLS_LOGO[language]?.logo }
           alt="language logo"
           width={24}
           height={24}
         />
+        :<></>
+        }
       </div>
       <div className="text-text-base text-base">{language}</div>
     </div>
