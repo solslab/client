5cd97cfb9ca25e32f7869eceb12d910ab5078d4f
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-14 17:53:04 +0900

    [Refactor] 로그삭제 및 리다이렉트 url변경

--- app/api/login/fail/route.ts
+++ app/api/login/fail/route.ts
@@ -1,7 +1,7 @@
+import { NEXT_URL } from "@/app/lib/constants";
 import { redirect } from "next/navigation";
 export async function GET(request:Request) {
-    const url = process.env.NEXT_URL || ''
-    console.log('로그인 실패')
-    redirect(url)
+
+    redirect('/')
     
 }
