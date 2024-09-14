bd56e6a24b13bdf642a0ecf7382150d20dba4c74
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-14 17:53:30 +0900

    [Refactor] 리다이렉트 url변경

--- app/api/login/success/route.ts
+++ app/api/login/success/route.ts
@@ -1,16 +1,23 @@
+import { NEXT_URL } from "@/app/lib/constants";
+import { infoCheck } from "@/app/lib/actions";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 export async function GET(request:Request) {
-    const baseUrl:string = process.env.NEXT_URL || '';
+
     const url = new URL(request.url);
-    const accessToken = url.searchParams.get('accessToken'); 
+    const accessToken = url.searchParams.get('accessToken') || ''; 
     const prevPath = cookies().get('sols-lastPath')
+    const infoTest = await infoCheck(accessToken);
     if (accessToken) {
         cookies().set('sols-accessToken', accessToken);
     }
-    if(prevPath){
-        redirect(baseUrl+prevPath.value)
+    if(!infoTest){
+        redirect(NEXT_URL + '/profiles/additional')
     }
-    redirect(baseUrl)
+    if(prevPath){
+        redirect(NEXT_URL +prevPath.value)
+    }
+    
+    redirect(NEXT_URL )
     
 }
