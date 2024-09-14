0fb6b174680f237dda11de24319cddc31de7a172
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-14 17:55:33 +0900

    [Fix] dom변경

--- app/testReview/page.tsx
+++ app/testReview/page.tsx
@@ -1,19 +1,12 @@
-import SmallContainer from "../ui/smallContainer";
-import { fetchProfile } from "../lib/data";
-import { Profile } from "../lib/definitions";
-import { findPlatformAndLabel } from "../lib/utils";
-import LanguageBox from "../ui/languageBox";
-import FieldBox from "../ui/fieldBox";
-import Input from "../ui/input";
 import XsContainer from "../ui/xsContainer";
 import TrForm from "../ui/trForm";
 
 export default async function Page() {
   return (
-    <main className="flex min-h-screen  justify-between ">
+    <div className="flex min-h-screen  justify-between pt-16 ">
       <XsContainer>
         <TrForm />
       </XsContainer>
-    </main>
+    </div>
   );
 }
