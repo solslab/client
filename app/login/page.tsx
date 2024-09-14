a769821b00294e8594e39c14b761ca61dd2e473e
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-14 17:59:08 +0900

    [Style] css변경

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Page() {
   // const router = useRouter()
@@ -15,26 +13,25 @@ export default function Page() {
   //   }
   // }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center py-24 bg-gradient-to-b from-login-start via-login-middle to-login-end">
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <Image
-          width={300}
-          height={300}
-          className="mx-auto"
-          src="/logoExam.png"
+          width={201}
+          height={101}
+          className="mx-auto w-auto h-auto"
+          src="/icons/logo_dark.png"
           alt="Sols"
         />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"></h2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <div>
           <Link
-            href="http://13.124.196.95:8080/oauth2/authorization/kakao"
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            href="https://solslab.site/api/oauth2/authorization/kakao"
+            className="flex  justify-center rounded-md   text-sm font-semibold  text-white shadow-sm"
           >
-            카카오로 시작하기
+            <Image src='/icons/kakao_login_medium_wide.png' alt='kakao button' width={300} height={45} />
           </Link>
         </div>
       </div>
