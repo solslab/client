0d78e6f651939ae6d1c782e64567c60a777a0171
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-14 17:45:58 +0900

    [Refactor] 인풋 컴포넌트 param추가

--- app/ui/input.tsx
+++ app/ui/input.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 
-export default function Input({ defaultValue,required,type }: { defaultValue?: string,required?:boolean,type?:string }) {
+export default function Input({ defaultValue,required,type,placeHolder,id,name }: { defaultValue?: string,required?:boolean,type?:string,placeHolder?:string;id?:string;name?:string}) {
 
   return (
     <div className="max-w-80 w-full">
       <input
-        className=" w-full border border-gray-50 px-2 py-1 rounded-lg"
+        id={id&& id}
+        name={name&& name}
+        className=" w-full border border-gray-50 px-2 py-1 rounded-lg shadow-customShadow"
         type={type}
         defaultValue={defaultValue}
         required={required}
+        placeholder={placeHolder&& placeHolder}
       />
     </div>
   );
