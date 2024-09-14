83d343a099651891ae11723dd704282de4ae54dc
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-14 17:44:29 +0900

    [Feat] 경로 쿠키 예외 추가

--- app/ui/lastPathSetter.tsx
+++ app/ui/lastPathSetter.tsx
@@ -1,17 +1,22 @@
 "use client";
 
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { usePathname} from "next/navigation";
 import {
   updateLastRoute,
 } from "@/app/lib/cookie";
 
-const exception = ["/login","/profiles/additional"];
+const exception = ["/login","/profiles/additional","/suggestion","/profiles/edit","/testReview"];
 
 export default function LastPathSetter() {
   const pathName = usePathname();
+  const containsException = useCallback((path:string)=> {
+    return exception.some(exceptionStr => path.includes(exceptionStr));
+},[])
+  
   useEffect(() => {
-    if (!exception.includes(pathName)) {
+    const isContain = containsException(pathName)
+    if (!isContain) {
       updateLastRoute(pathName);
     }
   }, [pathName]); // 라우트가 변경될 때마다 실행
