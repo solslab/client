3f52a137a7fee9bf42c1780c1e4c80d89bcd31c0
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-15 06:12:56 +0900

    [Feat] 애니메이션 추가

---
+++ app/ui/interaction/snow.tsx
@@ -0,0 +1,84 @@
+import Image from "next/image";
+
+export default function Snow(){
+return(
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-5/20 ">
+            <Image src='/companyLogo/naver.png' alt='naver logo' width={219} height={38} className="translate-x ml-96 w-auto h-auto "/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-2/20">
+            <Image src='/companyLogo/bc.png' alt='bd bank logo' width={140} height={141.44} className="-translate-x-100  w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-3/20">
+            <Image src='/companyLogo/carrot.png' alt='dang geun market logo' width={114} height={126} className="-translate-x-100 ml-72 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-4/20">
+            <Image src='/companyLogo/citi.png' alt='citi bank logo' width={140} height={138.57} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-5/20">
+            <Image src='/companyLogo/cj.png' alt='cj logo' width={114} height={95} className="-translate-x-100 w-auto ml-80 h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-6/20">
+            <Image src='/companyLogo/coupang.png' alt='coupang logo' width={238} height={51} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-7/20">
+            <Image src='/companyLogo/gabia.png' alt='gabia logo' width={220} height={98.06} className="-translate-x-100 ml-72 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-8/20">
+            <Image src='/companyLogo/hanaBank.png' alt='hana bank logo' width={105} height={91} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-9/20">
+            <Image src='/companyLogo/hyundaiCard.png' alt='hynunai card logo' width={220} height={42.01} className="-translate-x-100 ml-96 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-10/20">
+            <Image src='/companyLogo/ibk.png' alt='ibk logo' width={130} height={138.57} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-11/20">
+            <Image src='/companyLogo/kakao.png' alt='kakao logo' width={219} height={77} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-12/20">
+            <Image src='/companyLogo/kBank.png' alt='K bank logo' width={220} height={233.61} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-13/20">
+            <Image src='/companyLogo/kbBank.png' alt='Kb bank logo' width={140} height={140} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-14/20">
+            <Image src='/companyLogo/kt.png' alt='kt logo' width={95} height={77} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-15/20">
+            <Image src='/companyLogo/lg.png' alt='lg logo' width={230} height={230} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-16/20">
+            <Image src='/companyLogo/line.png' alt='line logo' width={161} height={162} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-17/20">
+            <Image src='/companyLogo/payco.png' alt='payco logo' width={130} height={131.34} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-18/20">
+            <Image src='/companyLogo/posco.png' alt='posco logo' width={210} height={56.68} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-19/20">
+            <Image src='/companyLogo/samsung.png' alt='samsung logo' width={220} height={73.33} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-4/20">
+            <Image src='/companyLogo/sk.png' alt='sk logo' width={180} height={180} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-8/10">
+            <Image src='/companyLogo/skTelecom.png' alt='Sk Telecom logo' width={152} height={53} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-12/10">
+            <Image src='/companyLogo/suhyup.png' alt='suhyup bank logo' width={140} height={140} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-16/10">
+            <Image src='/companyLogo/toss.png' alt='toss logo' width={285} height={142} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-7/10">
+            <Image src='/companyLogo/wanted.png' alt='wanted logo' width={152} height={68} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+            <div className="w-full animate-[logo_24s_linear_infinite] absolute left-0 top-17/10">
+            <Image src='/companyLogo/woori.png' alt='woori bank logo' width={130} height={130} className="-translate-x-100 w-auto h-auto"/>
+            </div>
+
+          </div>
+)
+}
