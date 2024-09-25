07da15f5e3ba3fc0a105afa231f07a1b36ca9ff7
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-25 23:49:44 +0900

    [Style] 기본 컨데이너 사이즈 변경

--- app/ui/container.tsx
+++ app/ui/container.tsx
@@ -4,5 +4,5 @@ export default function Container({
   children: React.ReactNode;
   className?:string;
 }>) {
-  return <div className={` w-full max-w-6xl  mx-auto px-4 ${className}`}>{children}</div>;
+  return <div className={` w-full max-w-5xl  mx-auto px-4 ${className}`}>{children}</div>;
 }
