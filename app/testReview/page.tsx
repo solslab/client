692bd7e8ef9eeb41a25dfb22664624e30e16f620
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-25 23:48:56 +0900

    [Feat] trlink 회사 id 전달

--- app/testReview/page.tsx
+++ app/testReview/page.tsx
@@ -1,11 +1,15 @@
 import XsContainer from "../ui/xsContainer";
 import TrForm from "../ui/trForm";
 
-export default async function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { company_id?: string; };
+}) {
   return (
     <div className="flex min-h-screen  justify-between pt-16 ">
       <XsContainer>
-        <TrForm />
+        <TrForm company_id={searchParams.company_id}/>
       </XsContainer>
     </div>
   );
