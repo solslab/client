acacf32c24216a0567d21dc1229ac5c05d597dff
tmddv <105033488+tmddv@users.noreply.github.com>
2024-09-25 23:49:23 +0900

    [Refactor] 네비게이션 최적화

--- app/ui/navigation/navSearchBox.tsx
+++ app/ui/navigation/navSearchBox.tsx
@@ -1,29 +1,30 @@
+'use client';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { fetchFilteredCompanys } from '@/app/lib/data';
 import { CompanyQuery } from '@/app/lib/definitions';
 import Link from 'next/link';
 import clsx from 'clsx';
-export default function NavSearchBox() {
+export default function NavSearchBox({ visible }: { visible?: boolean }) {
 	const [query, setQuery] = useState('');
 	const [value, setValue] = useState('');
 	const [companyList, setCompanyList] = useState<CompanyQuery[]>([]);
 
-    const clearFeild = ()=>{
-        setValue('');
-        setQuery('')
-        setCompanyList([]);
-    }
+	const clearFeild = () => {
+		setValue('');
+		setQuery('');
+		setCompanyList([]);
+	};
 
 	useEffect(() => {
-        if(value==''){
-			setQuery('')
-            setCompanyList([]);
-        }
+		if (value == '') {
+			setQuery('');
+			setCompanyList([]);
+		}
 		const handler = setTimeout(() => {
-            if(value!=''){
-                setQuery(value);
-            }
+			if (value != '') {
+				setQuery(value);
+			}
 		}, 200);
 
 		return () => {
@@ -31,55 +32,73 @@ export default function NavSearchBox() {
 		};
 	}, [value]);
 	useEffect(() => {
-    const fetchQuery = async () => {
+		const fetchQuery = async () => {
 			const data = await fetchFilteredCompanys(query);
 			setCompanyList(data);
 		};
-    if(query!=''){
-      fetchQuery();
-    }
+		if (query != '') {
+			fetchQuery();
+		}
 	}, [query]);
 
-
 	return (
-		<div className="relative  hidden sm:block">
-			<div className="relative">
-				<div className="pointer-events-none absolute inset-y-0 start-0 z-20 flex items-center ps-3">
-					<Image src="/icons/search.png" alt="search" width={16} height={16} />
-				</div>
-				<input
-					className={clsx(`block h-10 w-72 text-sm rounded-3xl border px-8 py-3 border-gray-50 bg-gray-5 focus:outline-none `,{
-						'border-b-0 outline-none bg-white rounded-b-none rounded-t-2.5xl':companyList.length>0
-					})}
-					type="text"
-					role="combobox"
-					aria-expanded="false"
-					placeholder={'기업을 검색해보세요!'}
-					onChange={(e) => {
-						setValue(e.target.value);
-					}}
-					value={value}
-				/>
-			</div>
-			<div>
-				<div className="absolute w-72">
-					<div className={clsx(`mx-auto max-h-56 w-full overflow-y-scroll  bg-white scrollbar-hide overflow-hidden`,
-						{
-							'border-x-gray-50 border-b-gray-50 border-x border-b rounded-b-2.5xl':companyList.length>0
-						}
-					)}>
-						{companyList &&
-							companyList.map((el: CompanyQuery) => (
-								<Link href={`/company/${el.company_id}`} key={el.company_id}>
-									<div onClick={clearFeild} className=" px-4 py-2 text-sm hover:bg-gray-100">{el.company_name}</div>
-								</Link>
-							))}
-							{
-								companyList.length>0&& <div onClick={clearFeild} className="fixed w-screen h-screen top-0 left-0 -z-10"></div>
-							}
+		<>
+			{visible ? (
+				<div className="relative hidden sm:block">
+					<div className="relative">
+						<div className="pointer-events-none absolute inset-y-0 start-0 z-20 flex items-center ps-3">
+							<Image src="/icons/search.png" alt="search" width={16} height={16} />
+						</div>
+						<input
+							className={clsx(
+								`block h-10 w-72 rounded-3xl border border-gray-50 bg-gray-5 px-8 py-3 text-sm focus:outline-none`,
+								{
+									'rounded-b-none rounded-t-2.5xl border-b-0 bg-white outline-none':
+										companyList.length > 0
+								}
+							)}
+							type="text"
+							role="combobox"
+							aria-expanded="false"
+							placeholder={'기업을 검색해보세요!'}
+							onChange={(e) => {
+								setValue(e.target.value);
+							}}
+							value={value}
+						/>
+					</div>
+					<div>
+						<div className="absolute w-72">
+							<div
+								className={clsx(
+									`mx-auto max-h-56 w-full overflow-hidden overflow-y-scroll bg-white scrollbar-hide`,
+									{
+										'rounded-b-2.5xl border-x border-b border-x-gray-50 border-b-gray-50 shadow-customShadow':
+											companyList.length > 0
+									}
+								)}
+							>
+								{companyList &&
+									companyList.map((el: CompanyQuery) => (
+										<Link href={`/company/${el.company_id}`} key={el.company_id}>
+											<div onClick={clearFeild} className="px-4 py-2 text-sm hover:bg-gray-100">
+												{el.company_name}
+											</div>
+										</Link>
+									))}
+								{companyList.length > 0 && (
+									<div
+										onClick={clearFeild}
+										className="fixed left-0 top-0 -z-10 h-screen w-screen"
+									></div>
+								)}
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
+			) : (
+				<></>
+			)}
+		</>
 	);
 }
--- app/ui/navigation/profileDropdown.tsx
+++ app/ui/navigation/profileDropdown.tsx
@@ -2,163 +2,211 @@
 
 import Link from 'next/link';
 import clsx from 'clsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import { logOut, tokenTest } from '@/app/lib/auth';
+import { usePathname } from 'next/navigation';
 
 const links = [{ name: '내 프로필', href: '/profiles' }];
 
 export default function ProfileDropdown({
 	userName,
-	logout
+	visible
 }: {
 	userName: string;
-	logout: () => void;
+	visible: boolean;
 }) {
+	const pathName = usePathname();
 	const [profileClicked, setProfileClicked] = useState(false);
+	const [loading, setLoading] = useState(true);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const logout = async () => {
+		await logOut(pathName);
+		setIsLoggedIn(false);
+	};
 	const handleProfileClicked = () => {
 		setProfileClicked(!profileClicked);
 	};
+	useEffect(() => {
+		async function checkToken() {
+			const cookieExist = await tokenTest();
+			if (cookieExist) {
+				setIsLoggedIn(true);
+			} else {
+				setIsLoggedIn(false);
+			}
+			setLoading(false);
+		}
+		checkToken();
+	}, []);
 
 	return (
 		<>
-			<button
-				type="button"
-				className="relative hidden rounded-full text-sm focus:outline-none sm:flex"
-				id="user-menu-button"
-				aria-expanded="false"
-				aria-haspopup="true"
-				onClick={() => handleProfileClicked()}
-			>
-				<span className="absolute -inset-1.5"></span>
-				<span className="sr-only">Open user menu</span>
-				<Image width={42} height={42} src="/icons/default_profile.png" alt="menu button" />
-			</button>
+			{loading ? (
+				<></>
+			) : isLoggedIn ? (
+				<>
+					{visible ? (
+						<button
+							type="button"
+							className="relative hidden rounded-full text-sm focus:outline-none sm:flex"
+							id="user-menu-button"
+							aria-expanded="false"
+							aria-haspopup="true"
+							onClick={() => handleProfileClicked()}
+						>
+							<span className="absolute -inset-1.5"></span>
+							<span className="sr-only">Open user menu</span>
+							<Image width={42} height={42} src="/icons/default_profile.png" alt="menu button" />
+						</button>
+					) : (
+						<button
+							type="button"
+							className="relative flex text-sm sm:hidden"
+							id="user-menu-button"
+							aria-expanded="false"
+							aria-haspopup="true"
+							onClick={() => handleProfileClicked()}
+						>
+							<span className="absolute -inset-1.5"></span>
+							<span className="sr-only">Open user menu</span>
+							<Image width={24} height={24} src="/icons/hamburger.png" alt="User avatar" />
+						</button>
+					)}
 
-			<button
-				type="button"
-				className="relative flex text-sm sm:hidden"
-				id="user-menu-button"
-				aria-expanded="false"
-				aria-haspopup="true"
-				onClick={() => handleProfileClicked()}
-			>
-				<span className="absolute -inset-1.5"></span>
-				<span className="sr-only">Open user menu</span>
-				<Image width={24} height={24} src="/icons/hamburger.png" alt="User avatar" />
-			</button>
-			<div
-				onClick={handleProfileClicked}
-				className={clsx(`fixed left-0 top-0 h-screen w-screen`, {
-					hidden: !profileClicked
-				})}
-			/>
+					<div
+						onClick={handleProfileClicked}
+						className={clsx(`fixed left-0 top-0 h-screen w-screen`, {
+							hidden: !profileClicked
+						})}
+					/>
 
-			<div
-				className={clsx(
-					`absolute right-0 z-10 mt-2 hidden w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:block`,
-					{
-						'sm:hidden': !profileClicked
-					}
-				)}
-				role="menu"
-				aria-orientation="vertical"
-				aria-labelledby="menu-button"
-				tabIndex={-1}
-			>
-				<div className="py-1 sm:block" role="none">
-					<div className="py-2" role="none">
+					{visible ? (
 						<div
-							className="flex items-center px-4 py-2 text-sm text-gray-700"
-							role="menuitem"
+							className={clsx(
+								`absolute right-0 z-10 mt-2 hidden w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:block`,
+								{
+									'sm:hidden': !profileClicked
+								}
+							)}
+							id="desktop menu"
+							role="menu"
+							aria-orientation="vertical"
+							aria-labelledby="menu-button"
 							tabIndex={-1}
-							id="menu-item-0"
 						>
-							<Image
-								className="rounded-full"
-								width={42}
-								height={42}
-								src="/icons/default_profile.png"
-								alt="User avatar"
-							/>
-							<div className="px-4 text-text-base">{userName}</div>
+							<div className="py-1 sm:block" role="none">
+								<div className="py-2" role="none">
+									<div
+										className="flex items-center px-4 py-2 text-sm text-gray-700"
+										role="menuitem"
+										tabIndex={-1}
+										id="menu-item-0"
+									>
+										<Image
+											className="rounded-full"
+											width={42}
+											height={42}
+											src="/icons/default_profile.png"
+											alt="User avatar"
+										/>
+										<div className="px-4 text-text-base">{userName}</div>
+									</div>
+								</div>
+								{links.map((link) => (
+									<Link
+										key={link.href + link.name}
+										href={link.href}
+										className="block px-4 py-2 text-sm text-text-base"
+										role="menuitem"
+										tabIndex={-1}
+									>
+										<div onClick={() => handleProfileClicked()}>{link.name}</div>
+									</Link>
+								))}
+							</div>
+							<div className="py-1" role="none">
+								<button
+									className="block px-4 py-2 text-sm text-text-base"
+									role="menuitem"
+									tabIndex={-1}
+									onClick={() => {
+										handleProfileClicked();
+										logout();
+									}}
+								>
+									로그아웃
+								</button>
+							</div>
 						</div>
-					</div>
-					{links.map((link) => (
-						<Link
-							key={link.href + link.name}
-							href={link.href}
-							className="block px-4 py-2 text-sm text-text-base"
-							role="menuitem"
-							tabIndex={-1}
-						><div	onClick={() => handleProfileClicked()}>
-							{link.name}
-						</div>
-						</Link>
-					))}
-				</div>
-				<div className="py-1" role="none">
-					<button
-						className="block px-4 py-2 text-sm text-text-base"
-						role="menuitem"
-						tabIndex={-1}
-						onClick={() =>{ 
-							handleProfileClicked();
-							logout()}}
-					>
-						로그아웃
-					</button>
-				</div>
-			</div>
-			<div
-				className={clsx(`fixed left-0 top-0 h-full w-full bg-white sm:hidden`, {
-					hidden: !profileClicked
-				})}
-				id="mobile-menu"
-			>
-				<div className="space-y-2 px-2 pb-3 pt-2 ">
-					<div className="h-44 w-full p-6 flex flex-col justify-between">
-						<div className='flex justify-between w-full'>
-							<Image
-								className="rounded-full"
-								width={74}
-								height={74}
-								src="/icons/default_profile.png"
-								alt="User avatar"
-							/>
-							<button onClick={handleProfileClicked} className='w-6 h-6 flex justify-center items-center'>
-								<Image width={16} height={16} src="/icons/ex.png" alt="cancel button" />
-							</button>
-						</div>
-						<div className='text-text-base text-xl'>{userName}</div>
-					</div>
-
-				</div>
-				<div id='' className='w-full bg-gray-10 h-1 my-5' ></div>
-				<div >
-				{links.map((link,index) => (
-						<Link
-							key={link.href + link.name}
-							href={link.href}
-
-							role="menuitem"
-							tabIndex={index}
+					) : (
+						<div
+							className={clsx(`fixed left-0 top-0 h-full w-full bg-white sm:hidden`, {
+								hidden: !profileClicked
+							})}
+							id="mobile-menu"
 						>
-							<div onClick={handleProfileClicked} className=" px-6 py-4 text-base text-text-base">{link.name}</div>
-
-						</Link>
-					))}
-					<div id='' className='w-full bg-gray-10 h-0.25 my-2' ></div>
-					<button
-						className="px-6 py-4 text-base text-text-base"
-						role="logOut"
-						tabIndex={-1}
-						onClick={() => {logout(); handleProfileClicked();}}
-					>
-						로그아웃
-					</button>
-
-				</div>
-			</div>
+							<div className="space-y-2 px-2 pb-3 pt-2">
+								<div className="flex h-44 w-full flex-col justify-between p-6">
+									<div className="flex w-full justify-between">
+										<Image
+											className="rounded-full"
+											width={74}
+											height={74}
+											src="/icons/default_profile.png"
+											alt="User avatar"
+										/>
+										<button
+											onClick={handleProfileClicked}
+											className="flex h-6 w-6 items-center justify-center"
+										>
+											<Image width={16} height={16} src="/icons/ex.png" alt="cancel button" />
+										</button>
+									</div>
+									<div className="text-xl text-text-base">{userName}</div>
+								</div>
+							</div>
+							<div id="" className="my-5 h-1 w-full bg-gray-10"></div>
+							<div>
+								{links.map((link, index) => (
+									<Link
+										key={link.href + link.name}
+										href={link.href}
+										role="menuitem"
+										tabIndex={index}
+									>
+										<div
+											onClick={handleProfileClicked}
+											className="px-6 py-4 text-base text-text-base"
+										>
+											{link.name}
+										</div>
+									</Link>
+								))}
+								<div id="" className="my-2 h-0.25 w-full bg-gray-10"></div>
+								<button
+									className="px-6 py-4 text-base text-text-base"
+									role="logOut"
+									tabIndex={-1}
+									onClick={() => {
+										logout();
+										handleProfileClicked();
+									}}
+								>
+									로그아웃
+								</button>
+							</div>
+						</div>
+					)}
+				</>
+			) : (
+				<Link
+					className="rounded-3xl border border-gray-40 px-6 py-2 text-sm font-medium text-text-base hover:text-black"
+					href="/login"
+				>
+					로그인 / 회원가입
+				</Link>
+			)}
 		</>
 	);
 }
