9eee7ab3f39ab90c4b1d083eab8ccbcddd3acbaa
tmddv <105033488+tmddv@users.noreply.github.com>
2024-10-09 00:19:53 +0900

    [Feat] 인덱스 메타 타이틀

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
-import Container from "./ui/container";
-import Float from "./ui/interaction/float";
-import ClientSearchBox from "./ui/clientSearchBox";
-import { Metadata } from "next";
-import Script from "next/script";
+import Container from './ui/container';
+import Float from './ui/interaction/float';
+import ClientSearchBox from './ui/clientSearchBox';
+import { Metadata } from 'next';
+import Script from 'next/script';
 
 const jsonLd = {
-  '@context': 'https://sols.kr',
-  '@type': 'WebSite',
-  name: '몇솔',
-  image: 'https://sols.kr/favicon.png',
-  description: '기업별 코딩테스트 정보를 한 번에. 지원자들의 100% 리얼 후기로 더 확실하게 대비하세요.',
-}
+	'@context': 'https://sols.kr',
+	'@type': 'WebSite',
+	name: '몇솔',
+	image: 'https://sols.kr/favicon.png',
+	description:
+		'기업별 코딩테스트 정보를 한 번에. 지원자들의 100% 리얼 후기로 더 확실하게 대비하세요.'
+};
 
 export const metadata: Metadata = {
 	title: '몇솔 | 개발자 취업 준비 필수 플랫폼',
 	description:
 		'기업별 코딩테스트 정보를 한 번에. 지원자들의 100% 리얼 후기로 더 확실하게 대비하세요.',
+	keywords:
+		'기업 코딩테스트, 코딩테스트 정보, 코딩테스트 후기, 프로그래머스, 개발자 취업, IT 기업 채용, 코딩테스트 언어, 코딩테스트 준비, 코딩테스트 합격, 코딩테스트 난이도, 비대면 코딩테스트, 개발자 채용 플랫폼',
 	icons: {
 		icon: '/favicon.png'
 	},
@@ -35,58 +38,54 @@ export const metadata: Metadata = {
 	}
 };
 
-
 export default function Home({
-  searchParams,
+	searchParams
 }: {
-  searchParams?: {
-    query?: string;
-  };
+	searchParams?: {
+		query?: string;
+	};
 }) {
-  const query = searchParams?.query || "";
-  let isFocused = false;
-  console.log(process.memoryUsage());
-  return (
-    <>
-     <Script
-     id="jsonLd"
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-      />
-      <div className="flex min-h-screen flex-col items-center justify-between">
-        <div className=" w-full h-screen flex justify-center items-center relative">
-          <Float/>
+	const query = searchParams?.query || '';
+	let isFocused = false;
+	console.log(process.memoryUsage());
+	return (
+		<>
+			<Script
+				id="jsonLd"
+				type="application/ld+json"
+				dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+			/>
+			<div className="flex min-h-screen flex-col items-center justify-between">
+				<div className="relative flex h-screen w-full items-center justify-center">
+					<Float />
 
-          <div className="w-full h-full flex justify-center items-center backdrop-blur-sm bg-white bg-opacity-65">
-          <Container className=" flex relative max-w-7xl h-46 justify-center">
-            <div className="flex flex-col justify-center items-center z-30 ">
-              <div>
-              <div className="text-3xl">
-                <div className=" pb-6 font-extrabold bg-gradient-text-2 text-center text-transparent bg-clip-text">
-                  더 빠르고, 쉽게
-                </div>
-                <div className="hidden sm:block pb-16 font-extrabold bg-gradient-text-2 text-center text-transparent bg-clip-text">
-                  기업 코딩테스트를 준비하는 방법
-                </div>
-                <div className=" block sm:hidden  pb-6 font-extrabold bg-gradient-text-2 text-center text-transparent bg-clip-text">
-                  기업 코딩테스트를 
-                </div>
-                <div className=" block sm:hidden  pb-16 font-extrabold bg-gradient-text-2 text-center text-transparent bg-clip-text">
-                  준비하는 방법
-                </div>
-                
-              </div>
+					<div className="flex h-full w-full items-center justify-center bg-white bg-opacity-65 backdrop-blur-sm">
+						<Container className="relative flex h-46 max-w-7xl justify-center">
+							<div className="z-30 flex flex-col items-center justify-center">
+								<div>
+									<div className="text-3xl">
+										<div className="bg-gradient-text-2 bg-clip-text pb-6 text-center font-extrabold text-transparent">
+											더 빠르고, 쉽게
+										</div>
+										<div className="hidden bg-gradient-text-2 bg-clip-text pb-16 text-center font-extrabold text-transparent sm:block">
+											기업 코딩테스트를 준비하는 방법
+										</div>
+										<div className="block bg-gradient-text-2 bg-clip-text pb-6 text-center font-extrabold text-transparent sm:hidden">
+											기업 코딩테스트를
+										</div>
+										<div className="block bg-gradient-text-2 bg-clip-text pb-16 text-center font-extrabold text-transparent sm:hidden">
+											준비하는 방법
+										</div>
+									</div>
 
-             <ClientSearchBox/>
-              </div>
-            </div>
-          </Container>
-          </div>
-        </div>
-        <div className='hidden lg:block'>
-        </div>
-
-      </div>
-    </>
-  );
+									<ClientSearchBox />
+								</div>
+							</div>
+						</Container>
+					</div>
+				</div>
+				<div className="hidden lg:block"></div>
+			</div>
+		</>
+	);
 }
