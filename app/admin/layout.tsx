52ae59441848a6e4c8e62b414f8616f8e73bb015
gyujh630 <rbwjrbxo@ajou.ac.kr>
2024-12-29 20:19:25 +0900

    [Feat] 관리자 기본 페이지

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,8 +1,14 @@
-'use client'
+'use client';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { AdminSidebar } from '@/app/admin/components/admin-sidebar';
+import { usePathname } from 'next/navigation';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+	const pathname = usePathname();
+
+	if (pathname === '/admin/login') {
+		return <>{children}</>;
+	}
 
 	return (
 		<SidebarProvider>
