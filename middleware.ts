8358ef99042b54ebeffee0dad4982da8ccfc2363
gyujh630 <rbwjrbxo@ajou.ac.kr>
2024-12-30 17:57:33 +0900

    [Feat] 서브도메인 미들웨어 인가처리

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,136 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { tokenTest } from './app/lib/auth';
+import { getLastRoute } from './app/lib/cookie';
+import { ADMIN_URL, NEXT_URL } from './app/lib/constants';
+import { getDateOneMonthLater } from './app/lib/utils';
+
+function setTokenCookie(response: NextResponse, token: string, cookieName: string) {
+	response.cookies.set(cookieName, token.replace('Bearer ', ''), {
+		httpOnly: true,
+		secure: true,
+		expires: getDateOneMonthLater()
+	});
+}
+
+export default async function middleware(request: NextRequest) {
+	const { nextUrl, headers } = request;
+	const { href: requestUrl, pathname: pathName } = nextUrl;
+	const hostname = headers.get('host');
+	const url = nextUrl.clone();
+
+	let token;
+	if (pathName.startsWith('/admin') || hostname === ADMIN_URL) {
+		token = await tokenTest('ADMIN');
+	} else {
+		token = await tokenTest('USER');
+	}
+
+	const lastPathCookie = await getLastRoute();
+	const lastPath = lastPathCookie?.value || '/';
+	let response;
+
+	// 관리자 인가처리(prod)
+	if (hostname === ADMIN_URL) {
+		url.pathname = `/admin${url.pathname}`; // 서브도메인 처리
+		if (pathName === '/login' || pathName === '/api/login') {
+			return NextResponse.rewrite(url);
+		}
+		if (token?.role === 'ADMIN') {
+			response = NextResponse.rewrite(url);
+			if (token.new_token) {
+				setTokenCookie(response, token.new_token, 'solslab-accessToken');
+			}
+		} else {
+			response = NextResponse.redirect(`https://${ADMIN_URL}/login`);
+			response.cookies.delete('solslab-accessToken');
+		}
+		return response;
+	}
+
+	// 관리자 인가처리(dev)
+	if (pathName.startsWith('/admin')) {
+		if (['/admin/login', '/admin/api/login'].includes(pathName)) {
+			return NextResponse.next();
+		}
+		if (token?.role === 'ADMIN') {
+			response = NextResponse.next();
+			if (token.new_token) {
+				setTokenCookie(response, token.new_token, 'solslab-accessToken');
+			}
+		} else {
+			response = NextResponse.redirect(`${NEXT_URL}/admin/login`);
+			response.cookies.delete('solslab-accessToken');
+		}
+		return response;
+	}
+
+	if (pathName.includes('suggestion')) {
+		if (token) {
+			response = NextResponse.next();
+			if (token.new_token) {
+				setTokenCookie(response, token.new_token, 'sols-accessToken');
+			}
+		} else {
+			response = NextResponse.redirect(`${NEXT_URL}/login`);
+			response.cookies.delete('sols-accessToken');
+		}
+		return response;
+	}
+
+	if (pathName.startsWith('/company')) {
+		if (token) {
+			if (token.new_token) {
+				response = NextResponse.next();
+				setTokenCookie(response, token.new_token, 'sols-accessToken');
+			}
+		} else {
+			response = NextResponse.redirect(requestUrl);
+			response.cookies.delete('sols-accessToken');
+		}
+		return response;
+	}
+
+	if (pathName.startsWith('/profiles')) {
+		if (token) {
+			response = NextResponse.next();
+			if (token.new_token) {
+				setTokenCookie(response, token.new_token, 'sols-accessToken');
+			}
+			if (pathName.startsWith('/profiles/additional')) {
+				response = NextResponse.redirect(`${NEXT_URL}${lastPath}`, { status: 308 });
+			}
+		} else {
+			response = NextResponse.redirect(`${NEXT_URL}/login`);
+			response.cookies.delete('sols-accessToken');
+		}
+		return response;
+	}
+
+	if (pathName.startsWith('/login')) {
+		if (token) {
+			response = NextResponse.redirect(`${NEXT_URL}${lastPath}`);
+		} else {
+			response = NextResponse.next();
+			response.cookies.delete('sols-accessToken');
+		}
+		return response;
+	}
+
+	if (token && token.new_token) {
+		response = NextResponse.next();
+		setTokenCookie(response, token.new_token, 'sols-accessToken');
+		return response;
+	} 
+
+	return NextResponse.next();
+}
+
+
+
+
+
+/* 이전 코드 - matcher 제거 */
+
 // import { NextRequest, NextResponse } from 'next/server';
 // import { tokenTest } from './app/lib/auth';
 // import { infoCheck } from './app/lib/actions';
@@ -160,147 +293,5 @@
 // 		'/login',
 // 		'/member/:path*',
 // 		'/company/:path*'
-// 	]
-// };
-
-import { NextRequest, NextResponse } from 'next/server';
-import { tokenTest } from './app/lib/auth';
-import { getLastRoute } from './app/lib/cookie';
-import { ADMIN_URL, NEXT_URL } from './app/lib/constants';
-import { getDateOneMonthLater } from './app/lib/utils';
-
-function setTokenCookie(response: NextResponse, token: string, cookieName: string) {
-	response.cookies.set(cookieName, token.replace('Bearer ', ''), {
-		httpOnly: true,
-		secure: true,
-		expires: getDateOneMonthLater()
-	});
-}
-
-export default async function middleware(request: NextRequest) {
-	const { nextUrl, headers } = request;
-	const { href: requestUrl, pathname: pathName } = nextUrl;
-	const hostname = headers.get('host');
-	const url = nextUrl.clone();
-
-	let token;
-	if (pathName.startsWith('/admin') || hostname === ADMIN_URL) {
-		token = await tokenTest('ADMIN');
-	} else {
-		token = await tokenTest('USER');
-	}
-
-	const lastPathCookie = await getLastRoute();
-	const lastPath = lastPathCookie?.value || '/';
-	let response;
-
-	if (hostname === ADMIN_URL) {
-		url.pathname = `/admin${url.pathname}`;
-		if (pathName === '/login' || pathName === '/api/login') {
-			return NextResponse.rewrite(url);
-		}
-		if (token?.role === 'ADMIN') {
-			response = NextResponse.rewrite(url);
-			if (token.new_token) {
-				setTokenCookie(response, token.new_token, 'solslab-accessToken');
-			}
-		} else {
-			response = NextResponse.redirect(`https://${ADMIN_URL}/login`);
-			response.cookies.delete('solslab-accessToken');
-		}
-		return response;
-	}
-
-	if (pathName.startsWith('/admin')) {
-		if (['/admin/login', '/admin/api/login'].includes(pathName)) {
-			return NextResponse.next();
-		}
-		if (token?.role === 'ADMIN') {
-			response = NextResponse.next();
-			if (token.new_token) {
-				setTokenCookie(response, token.new_token, 'solslab-accessToken');
-			}
-		} else {
-			response = NextResponse.redirect(`${NEXT_URL}/admin/login`);
-			response.cookies.delete('solslab-accessToken');
-		}
-		return response;
-	}
-
-	if (pathName.includes('suggestion')) {
-		if (token) {
-			response = NextResponse.next();
-			if (token.new_token) {
-				setTokenCookie(response, token.new_token, 'sols-accessToken');
-			}
-		} else {
-			response = NextResponse.redirect(`${NEXT_URL}/login`);
-			response.cookies.delete('sols-accessToken');
-		}
-		return response;
-	}
-
-	if (pathName.startsWith('/company')) {
-		if (token) {
-			if (token.new_token) {
-				response = NextResponse.next();
-				setTokenCookie(response, token.new_token, 'sols-accessToken');
-			}
-		} else {
-			response = NextResponse.redirect(requestUrl);
-			response.cookies.delete('sols-accessToken');
-		}
-		return response;
-	}
-
-	if (pathName.startsWith('/profiles')) {
-		if (token) {
-			response = NextResponse.next();
-			if (token.new_token) {
-				setTokenCookie(response, token.new_token, 'sols-accessToken');
-			}
-			if (pathName.startsWith('/profiles/additional')) {
-				response = NextResponse.redirect(`${NEXT_URL}${lastPath}`, { status: 308 });
-			}
-		} else {
-			response = NextResponse.redirect(`${NEXT_URL}/login`);
-			response.cookies.delete('sols-accessToken');
-		}
-		return response;
-	}
-
-	if (pathName.startsWith('/login')) {
-		console.log('hi')
-		console.log(lastPath);
-		if (token) {
-			console.log('hi2');
-			response = NextResponse.redirect(`${NEXT_URL}${lastPath}`);
-		} else {
-			response = NextResponse.next();
-			console.log('hi3');
-			response.cookies.delete('sols-accessToken');
-		}
-		return response;
-	}
-
-	if (token && token.new_token) {
-		response = NextResponse.next();
-		setTokenCookie(response, token.new_token, 'sols-accessToken');
-		return response;
-	} 
-
-	return NextResponse.next();
-}
-
-// export const config = {
-// 	matcher: [
-// 		'/admin/:path*',
-// 		'/company/:id*',
-// 		'/profiles/:path*',
-// 		'/testReview',
-// 		'/login',
-// 		'/member/:path*',
-// 		'/company/:path*',
-// 		'/api/:path*'
 // 	]
 // };
