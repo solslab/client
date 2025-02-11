1bc326441f09a86ae369119f3d52817cd96d1e1c
tmddv <105033488+tmddv@users.noreply.github.com>
2025-02-11 20:16:34 +0900

    [Fix] 홈 리뷰링크 blink 수정

--- app/ui/main/IndexTrLink.tsx
+++ app/ui/main/IndexTrLink.tsx
@@ -5,40 +5,37 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import TierModal from '../company/tierModal';
 
-export default function IndexTrLink({company_id}:{company_id?:string}) {
-  const[modalVisible,setModalVisible] = useState(false);
-  const router = useRouter()
+export default function IndexTrLink({ company_id }: { company_id?: string }) {
+	const [modalVisible, setModalVisible] = useState(false);
+	const router = useRouter();
 
 	return (
 		<>
-			<div
-            className='flex justify-center flex-row'
-			>
-				<button onClick={async () => {
-					const tokenCookie = await getToken();
-					const token = tokenCookie?.value || undefined;
-					if (!token) {
-						{
-							router.push('/login')
+			<div className="flex flex-row justify-center">
+				<button
+					onClick={async () => {
+						const tokenCookie = await getToken();
+						const token = tokenCookie?.value || undefined;
+						if (!token) {
+							{
+								router.push('/login');
+							}
+						} else {
+							const infoChecked = await infoCheck(token);
+							if (infoChecked) {
+								router.push(`/testReview${company_id ? '?company_id=' + company_id : ''}`);
+							} else {
+								setModalVisible(true);
+							}
 						}
-					}
-					const infoChecked = await infoCheck(token);
-					if (infoChecked) {
-						router.push(`/testReview${company_id?'?company_id='+company_id:''}`)
-					}
-          else{
-           setModalVisible(true);
-          }
-				}} type="button" className="rounded-[10px] border-2 border-main-base px-3 py-2 text-center font-bold text-main-base md:px-6 md:py-4">
+					}}
+					type="button"
+					className="rounded-[10px] border-2 border-main-base px-3 py-2 text-center font-bold text-main-base md:px-6 md:py-4"
+				>
 					코딩테스트 후기 작성하기
 				</button>
 			</div>
-      {
-        modalVisible?
-        <TierModal setVisible={setModalVisible}/>
-        :
-        <></>
-      }
+			{modalVisible ? <TierModal setVisible={setModalVisible} /> : <></>}
 		</>
 	);
 }
