13fea8021c03f2cbce79d1147d0d1ca05f4ab094
한규정 <rbwjrbxo@ajou.ac.kr>
2025-02-12 11:04:00 +0900

    [Fix] 사이트맵 uri 도메인 수정

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,13 +17,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap>  {
   const companies: Ids[] = await fetchAllCompanies();
   const newSiteMaps:Sitemap[]= companies.flatMap(company => [
     {
-      url: `/company/${company.company_id}`,
+      url: `https://sols.kr/company/${company.company_id}`,
       lastModified: new Date().toISOString(),
       priority: 1,
       changeFrequency: 'daily'
     },
     {
-      url: `/company/${company.company_id}/datalab`,
+      url: `https://sols.kr/company/${company.company_id}/datalab`,
       lastModified: new Date().toISOString(),
       priority: 1,
       changeFrequency: 'daily'
